f6c2c4533f39e42d11b75fc0d72ae056df511d50
napcae <napcae@users.noreply.github.com>
2025-12-18 06:37:20 +0000

    deploy: cde136926409af6684f0be0f59e79c47300293cf

---
+++ _next/static/chunks/app/layout-d00efbe5b5bdf6c7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{494:()=>{},545:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,494,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[287,437,664,358],()=>s(545)),_N_E=e.O()}]);
